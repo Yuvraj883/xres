3279ea356b22f68ca09507475ab617885ff2fca1
Yuvraj <kingyuvi88@gmail.com>
2023-08-12 23:22:29 +0530

    Added custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans'], // Add your custom font name here
+      },
+    },
   },
   plugins: [],
 }
