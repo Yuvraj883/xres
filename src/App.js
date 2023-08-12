c651fe8acd5c7a9c8c76d0b8725b4d5e3737c5ec
Yuvraj <kingyuvi88@gmail.com>
2023-08-12 16:37:35 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Hero from "./components/Hero";
+import Model from "./components/Model";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
    <>
-   <h1>React App</h1>
+   {/* <Model/> */}
+   <Navbar/>
+   <Hero/>
+   
    </>
   );
 }
