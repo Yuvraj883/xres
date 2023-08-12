d47c5513952b5994fbabc07b44a22c0ea7fdda2c
Yuvraj <kingyuvi88@gmail.com>
2023-08-12 16:37:11 +0530

    Created Model component

---
+++ src/components/Model.jsx
@@ -0,0 +1,16 @@
+// const Model = ()=>{
+//     return(
+//         <>
+//             <div className="bg-gray-200 h-[600px] p-2">
+//                 <div className="bg-yellow-200 h-[580px]">
+//                     <div>
+
+//                     </div>
+
+//                 </div>
+                
+//             </div>
+//         </>
+//     )
+// }
+// export default Model; 
