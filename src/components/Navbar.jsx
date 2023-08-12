ff91c483b0f93c3b7b10a0d7fe676ae4f251eb1c
Yuvraj <kingyuvi88@gmail.com>
2023-08-13 03:38:25 +0530

    Updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const backgroundImage = `url('./assets/linear-gradient-shadow.png')`;
+  // const backgroundImage = `url('../assets/linear-gradient-shadow.png')`;
 
   return (
-   
-      <div style={{backgroundImage}} className="container">
-        <div className="flex flex-row bg-white justify-between px-4 mx-8 my-4 items-center shadow-2xl rounded-full -shadow-3xl">
+   <>   
+     <img className="relative h-48 w-[100%] mb-[-10%]" src={require('../assets/background-img.png')} alt="shadow"/>
+        <div className=" absolute w-[90%] top-5 flex flex-row bg-white justify-between px-4 mx-8  items-center shadow-2xl rounded-full -shadow-3xl">
         <Link to="/">
                     <span className="font-bold text-xl">XRES</span>
                     </Link>
           <ul className="flex font-semibold">
 
-            <Link to="/"> <li className="p-2 cursor-pointer">Home</li></Link>
-            <li className="p-2 cursor-pointer">Features</li>
-            <li className="p-2 cursor-pointer">Sign Up</li>
-            <li className="p-2 cursor-pointer">
+            <Link to="/"> <li className="px-10 py-2 cursor-pointer">Home</li></Link>
+            <li className="py-2 px-10 cursor-pointer">Features</li>
+            <li className="py-2 px-10 cursor-pointer">Sign Up</li>
+            <li className="py-2 px-4 cursor-pointer">
               <img
                 className="h-8 rounded-full"
                 src="https://s3-alpha-sig.figma.com/img/73a7/2128/55a716ca3016df129585dc84ce1b84f3?Expires=1692576000&Signature=HzS4jJRCFKwKLZReji0bBybtlwo5iPoPYIK14okewtN-OQ7Wc0gLlN34Erdw8PabE8n1bP-qbd4UQz0rHgST4BsYOsFRQpXey3QwSjoLKc7Ffiz7-oxVNorHo~dLINiwgCNADWULbgsrFtqn6nW8s7YyEBjJdbRZOtLPxGcXn9wPFtZCVulW3hi-KSYbcJ5nU3eoo2LPZVUOa3JWyDfqU6Ezvxeam5-qCQbu-F4uGq-M9qh4rSVXpwbYzX3aCVzpngY9qVcRE0zVsrPD9hK5WkKLCgzaQJt9pHMFxLTOsuuJlQM5IFaKHghsUiPIDODGngHRU6p421s6n~Fpx3wZKg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
@@ -24,7 +24,8 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-      </div>
+      
+      </>
     
   );
 };
