9af7b487077e12f80ba0745dc2b7c80be189896b
Yuvraj <kingyuvi88@gmail.com>
2023-08-12 20:31:00 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Solutions from "./Solutions";
+import GetStarted from "./components/GetStarted";
 import Hero from "./components/Hero";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
@@ -7,11 +8,13 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
    <>
+   <GetStarted/>
+
    <Model/> 
    <Navbar/>
    <Hero/>
    <Solutions/>
-   
+
    
    </>
   );
