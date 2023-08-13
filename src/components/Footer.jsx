ace907d21df26f11d6e9241a71f9c393e37a76f3
Yuvraj <kingyuvi88@gmail.com>
2023-08-13 06:01:13 +0530

    Created Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = ()=>{
+    return(
+        <>
+        <div className="text-center">
+            <hr className="mx-auto border-1 w-[80%] border-black"/>
+            <p className="p-2">Made with love by <b>Team X.E.R.S.</b></p>
+            </div>
+        </>
+    )
+}
+export default Footer; 
